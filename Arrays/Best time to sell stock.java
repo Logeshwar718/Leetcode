42a56cf46ddb6115bf3f64a8dd27d226f96a1d26
logeshwarks <logeshwarks2005@gmail.com>
2026-03-18 20:40:50 +0530

    best time to sell stock

---
+++ Arrays/Best time to sell stock.java
@@ -0,0 +1,12 @@
+package Arrays;
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0], profit=0, maxProfit=0;
+        for(int i=1;i<prices.length;i++){
+            min=Math.min(min,prices[i]);
+            profit=prices[i]-min;
+            maxProfit=Math.max(maxProfit,profit);
+        }
+        return maxProfit;
+    }
+}
