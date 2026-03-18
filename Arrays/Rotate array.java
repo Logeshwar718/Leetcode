fbcba7442f128dc2090b13cec7959eb76b56c46a
LOGESHWAR K S <logeshwar64@gmail.com>
2026-03-18 20:51:17 +0530

    added a variable for length of the array

--- Arrays/Rotate array.java
+++ Arrays/Rotate array.java
@@ -1,10 +1,11 @@
 package Arrays;
 class Solution {
     public void rotate(int[] nums, int k) {
-        k=k%nums.length;
-        reverse(nums,0,nums.length-1);
+        int n=nums.length;
+        k=k%n;
+        reverse(nums,0,n-1);
         reverse(nums,0,k-1);
-        reverse(nums,k,nums.length-1);
+        reverse(nums,k,n-1);
     }
     static void reverse(int[] arr,int start,int end){
         while(start<end){
